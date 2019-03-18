0366e82c2aee835eeeceae40a3f8b55cfa97c244
Larry Samuels <larry_samuels@hotmail.com>
2019-03-18 18:33:19 -0400

    Add blog model and admin for it

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Create blog model
+# title 
+# pub_date 
+# body 
+# image 
+class Blog(models.Model):
+    title = models.CharField(max_length = 255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+# add blog app to the settings
+
+# create a migration
+
+# migrage
+
+# add to admin 
+
+
--- portfolio/jobs/admin.py
+++ portfolio/jobs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Job 
+from .models import Job
 
 admin.site.register(Job)
 
