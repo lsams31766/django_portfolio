1a78e3a2acca9e384579e14bb6f0e9215ebe6d1e
Larry Samuels <larry_samuels@hotmail.com>
2019-03-26 15:43:28 -0400

    .gitignore fix

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -10,14 +10,14 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
-
-
-# add blog app to the settings
-
-# create a migration
-
-# migrage
-
-# add to admin 
+    
+    def summary(self):
+        return self.body[0:100]
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+    
+    def __str__(self):
+        return self.title 
 
 
---
+++ portfolio/blog/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.allblogs, name='allblogs'),
+    #look for int after /blog
+    path('<int:blog_id>/', views.detail, name='detail'),
+]
